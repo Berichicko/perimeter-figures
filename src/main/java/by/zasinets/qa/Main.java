b2fd39ffbfb6e115368c31e6e641e89aa764314f
Aliaksandr Zasinets <aliaksandr.zasinets@gmail.com>
2022-06-18 20:01:12 +0300

    Removed extra code and added a text perimeter

--- src/main/java/by/zasinets/qa/Main.java
+++ src/main/java/by/zasinets/qa/Main.java
@@ -2,18 +2,11 @@ package by.zasinets.qa;
 
 public class Main {
     public static void main(String[] args) {
-        Circle circle = new Circle("black",3);
-        Rectangle rectangle = new Rectangle("red",3,3);
-        Triangle triangle = new Triangle("blue",4,4);
-
-        System.out.println(circle.calculateArea());
-        System.out.println(rectangle.calculateArea());
-        System.out.println(triangle.calculateArea());
-
-        System.out.println(circle.perimeter());
-        System.out.println(rectangle.perimeter());
-        System.out.println(triangle.perimeter());
-
-
+        Circle circle = new Circle("black", 3);
+        Rectangle rectangle = new Rectangle("red", 3, 3);
+        Triangle triangle = new Triangle("blue", 4, 4);
+        System.out.println("Perimeter of circle: " + circle.perimeter());
+        System.out.println("Perimeter of rectangle: " + rectangle.perimeter());
+        System.out.println("Perimeter of triangle: " + triangle.perimeter());
     }
 }
